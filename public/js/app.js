f6f7966f6c34476083bf0bcdd3e678a3523b3b14
lynda.bounif <lynda.bounif@tech-instinct.com>
2019-03-13 10:52:25 +0100

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading ...'
+    messageTwo.textContent = ' '
+
+    fetch('http://localhost:3000/weather?address='+ location).then((response)=>
+    {
+        response.json().then((data) => {
+            if(data.error){
+                //console.log(data.error)
+                messageOne.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.location
+
+               // messageTwo.textContent = 'Summary: ' + JSON.stringify(data.forcast.summary) + 
+               //messageTwo.textContent = JSON.stringify(data.forcast)
+
+                console.log(data.location)
+
+                console.log(typeof(messageTwo))
+               // const summary = JSON.stringify(data.forcast.summary)
+                //const temperature = data.forcast.temperature
+               // const rain = data.forcast.rain
+               // console.log(typeof(rain))
+                 messageTwo.textContent = 'Summary: ' + data.forcast.summary + ' \nTemperature: '+ data.forcast.temperature +' \nRain: '+ data.forcast.rain 
+                //console.log(JSON.stringify(data.forcast))
+                //console.log('Summary: '+ summary, '\n temperature: '+ temperature, '\n rain: '+ rain)
+                //messageTwo = summary
+               // messageTwo = 'Summary: '+ summary +'  temperature: '+ temperature+ ' rain: '+ rain
+            }
+        })
+    })
+
+    console.log(location)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+//const {temperature,precipProbability:rain} = response.body.currently
+//const {summary} = response.body.daily.data[0]
+
+const forecast = (lat,long,callback)=>{
+    const url = 'https://api.darksky.net/forecast/c00caeece3bb0746e2a4b5566fc05dde/'+lat +','+ long +'?units=si&lang=fr'
+    request({url, json:true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect to location', undefined)
+        }
+        else if(body.error){
+            callback('Unable to find location',undefined)
+        }
+        else{
+            callback(undefined, {
+                summary: body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                rain: body.currently.precipProbability
+
+            })
+        }
+    })
+}
+
+module.exports = forecast
