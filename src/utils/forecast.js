8c28dbc565d82c14ad4350dc35012ca1bb3fabdb
lynda.bounif <lynda.bounif@tech-instinct.com>
2019-03-13 14:48:35 +0100

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const request = require('request')
 //const {summary} = response.body.daily.data[0]
 
 const forecast = (lat,long,callback)=>{
-    const url = 'https://api.darksky.net/forecast/c00caeece3bb0746e2a4b5566fc05dde/'+lat +','+ long +'?units=si&lang=fr'
+    const url = 'https://api.darksky.net/forecast/c00caeece3bb0746e2a4b5566fc05dde/'+lat +','+ long +'?units=si'
     request({url, json:true}, (error, {body}) => {
         if(error){
             callback('Unable to connect to location', undefined)
@@ -17,7 +17,6 @@ const forecast = (lat,long,callback)=>{
                 summary: body.daily.data[0].summary,
                 temperature: body.currently.temperature,
                 rain: body.currently.precipProbability
-
             })
         }
     })
